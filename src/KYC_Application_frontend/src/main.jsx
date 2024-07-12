38d992d43f70d2727d74d7ef3526b8888cddc6d2
Abdullah-1057 <abdullah.hkt@gmail.com>
2024-07-12 14:47:32 +0500

    push

--- src/KYC_Application_frontend/src/main.jsx
+++ src/KYC_Application_frontend/src/main.jsx
@@ -9,11 +9,15 @@ import Spinner from "./views/spinner/Spinner";
 import "./_mockApis";
 import "./utils/i18n";
 import { InternetIdentity } from "@connect2ic/core/providers/internet-identity";
+import { InfinityWallet } from "@connect2ic/core/providers/infinity-wallet";
 import { createClient } from "@connect2ic/core";
 import { Connect2ICProvider } from "@connect2ic/react";
 import "@connect2ic/core/style.css";
 const client = createClient({
-  providers: [new InternetIdentity()],
+  providers: [new InternetIdentity(), new InfinityWallet()],
+  // canisters: {
+  //   myCanister
+  // }
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
