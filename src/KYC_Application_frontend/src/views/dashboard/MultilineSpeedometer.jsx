a5ac54b24540cd7c69a1f48f8c7e165be7ef9cbc
Ubaid-developer <ubaidfsd234@gmail.com>
2025-01-15 17:00:32 +0500

    push

--- src/KYC_Application_frontend/src/views/dashboard/MultilineSpeedometer.jsx
+++ src/KYC_Application_frontend/src/views/dashboard/MultilineSpeedometer.jsx
@@ -3,28 +3,25 @@ import ReactSpeedometer from 'react-d3-speedometer';
 import * as d3 from 'd3';
 
 function MultilineSpeedometer({ value }) {
-  const [renderKey, setRenderKey] = useState(0);
-
-  useEffect(() => {
-    setRenderKey(prevKey => prevKey + 1); // Increment key to force re-render
-  }, [value]);
 
   useEffect(() => {
     setTimeout(() => {
-      const text = d3.select(`.speedometer-${renderKey} .current-value`);
+      const text = d3.select('.speedometer .current-value');
       if (text.empty()) return;
 
       text.text(''); // Clear existing text
       text.append('tspan')
-        .attr('x', 0)
-        .attr('dy', '-0.6em')
-        .text('Fair');
+      .attr('x', 0)
+      .attr('dy', '-3.4em') // Move text up
+      .style('font-size', '16px') // Increase font size
+      .text('Fair');
       text.append('tspan')
-        .attr('x', 0)
-        .attr('dy', '1.2em')
-        .text(value);
+      .attr('x', 0)
+      .attr('dy', '1.2em') // Position next line appropriately
+      .style('font-size', '46px') // Consistent font size for the number
+      .text(value);
     }, 0);
-  }, [renderKey, value]);
+  }, [ value]);
 
   return (
     <div style={{ transform: 'scale(0.85)', transformOrigin: 'center' }}> {/* Scale down the speedometer */}
@@ -33,15 +30,15 @@ function MultilineSpeedometer({ value }) {
         maxSegmentLabels={1}
         customSegmentStops={[300, 680, 800]}
         segmentColors={['#ffa90f', '#f6f8fa']}
-        needleColor={'#fff'}
+        needleColor="rgba(0,0,0,0)" 
         currentValueText=" " // Intentionally blank
         minValue={300}
         maxValue={800}
         value={value}
         ringWidth={15}
         textColor={'#000'}
-        width={300} // Adjust the width to control size
-        className={`speedometer-${renderKey}`} // Unique class name on each render
+        width={300} // Adjust the width to control size}
+      className="speedometer"
       />
     </div>
   );
